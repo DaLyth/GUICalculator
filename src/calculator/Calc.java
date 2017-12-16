9942990ef764c8b40d4548daf9f2837d6e4526a9
DaLyth <notify.dany@gmail.com>
2017-12-16 15:38:14 -0500

    Minor changes

--- src/calculator/Calc.java
+++ src/calculator/Calc.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package calculator;
 
 import java.math.BigInteger;
@@ -10,7 +5,7 @@ import java.util.Scanner;
 
 /**
  *
- * @author Dany Lyth
+ * @author DaLyth
  */
 public class Calc extends javax.swing.JFrame {
 
